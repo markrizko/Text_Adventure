379cb7b5dcb2d01692f287ea012857a2c751802a
markrizko <mark@rizkocircle.com>
2016-05-20 10:10:15 -0700

    Printed Intro and Integrated si3

--- Assets/TextController.cs
+++ Assets/TextController.cs
@@ -14,13 +14,15 @@ public class TextController : MonoBehaviour {
 	void Update ()
 	{
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			text.text = "Space Key Pressed.";
-		}
-		else if (Input.GetKeyDown (KeyCode.Return)) {
-			text.text = "Return Key Pressed.";
-		}
-		else if (Input.GetKeyDown (KeyCode.Tab)) {
-			text.text = "Tab Key Pressed.";
+			text.text = "It's been 34 days since the incident, seven million dollars " + 
+				"embezzled and every trace of evidence points back to you. Your know" +
+				"you're incident, but you have no proof. Your fingerprints were on the " +
+				"keyboard on the computer that the money was stolen on. You need to escape " +
+				"from prison, investigate the scene on your own and gather evidence, and break " +
+				"back into prison before dawn. You're the only hope for your family. "+
+				"All you have in your cell are some dirty sheets, a file, and the barred door" +
+				"seems to be locked from the outside.\n\nPress S to view the sheets, F to" + 
+				"view the file, and D to view the door.";
 		}
 	}
-}
+} 
